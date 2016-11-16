088706ea677ba9dbbebb4a80b87e6cf419d581a8
My Name <my@email.com>
2016-11-16 23:05:06 +0300

    rm nginx default;

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
