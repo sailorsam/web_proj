7e83ef96a1bc65f031bf8ee4ed45e1aa466ed2fb
Semen Kuzmin <kuzmin@ingenico.ru>
2016-11-22 18:34:35 +0300

    added db support;

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    rating = models.IntegerField()
+    added_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, related_name="question_author")
+    likes = models.ManyToManyField(User)
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now=True)
+    question = models.ForeignKey(Question)
+author = models.ForeignKey(User, related_name="answer_author")
--- init.sh
+++ init.sh
@@ -6,5 +6,6 @@ gunicorn -b 0.0.0.0:8080 --pythonpath /home/box/web/ hello:app&
 gunicorn -b 0.0.0.0:8000 --pythonpath /home/box/web/ask ask.wsgi&
 #sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 #sudo /etc/init.d/gunicorn restart
-#sudo /etc/init.d/mysql start
+/etc/init.d/mysql restart
+mysql -uroot -e "create database django"
 echo "Ready!"
